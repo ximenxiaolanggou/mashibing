f271c00d5821059e202e6461f77cbf7ecb9d6864
“damoncai” <782099197@qq.com>
2023-11-06 16:35:03 +0800

    静态常量

---
+++ 26_JVM/src/main/java/center/hellowrold/jvm/chapter08/Demo_01_StaticFinal.java
@@ -0,0 +1,17 @@
+package center.hellowrold.jvm.chapter08;
+
+/**
+ * @author zhishun.cai
+ * @create 2023/11/6
+ * @note
+ */
+public class Demo_01_StaticFinal {
+
+    static int a = 1;
+    static final  int b = 1;
+
+    public static void main(String[] args) {
+
+    }
+
+}
