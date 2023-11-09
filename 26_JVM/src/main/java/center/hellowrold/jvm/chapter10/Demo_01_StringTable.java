4a2e630dfeea23f93737572bafe9504145de41f4
“damoncai” <782099197@qq.com>
2023-11-09 17:05:52 +0800

    dump

---
+++ 26_JVM/src/main/java/center/hellowrold/jvm/chapter10/Demo_01_StringTable.java
@@ -0,0 +1,25 @@
+package center.hellowrold.jvm.chapter10;
+
+/**
+ * @author zhishun.cai
+ * @create 2023/11/8
+ * @note
+ */
+public class Demo_01_StringTable {
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+
+
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+    }
+}
---
+++ 26_JVM/src/main/java/center/hellowrold/jvm/chapter10/Demo_02_StringTable_2.java
@@ -0,0 +1,14 @@
+package center.hellowrold.jvm.chapter10;
+
+/**
+ * @author zhishun.cai
+ * @create 2023/11/8
+ * @note
+ */
+public class Demo_02_StringTable_2 {
+
+    public static void main(String[] args) {
+        String s = new String("a") + new String("b");
+        System.out.println();
+    }
+}
